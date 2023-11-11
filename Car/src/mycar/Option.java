52a2e1a9ae4cb57228639aa16101c35f198175af
jahee <jahee0128@naver.com>
2023-11-11 23:42:55 +0900

    test

---
+++ Car/src/mycar/Option.java
@@ -0,0 +1,27 @@
+package mycar;
+
+import java.util.Scanner;
+
+public class Option implements Manageable{
+	String name;
+	int price;
+
+	@Override
+	public void read(Scanner scan) {
+		name = scan.next();
+		price = scan.nextInt();
+
+	}
+
+	@Override
+	public void print() {
+		System.out.printf("%s %d만원\n", name,price);
+	}
+
+	@Override
+	public boolean matches(String kwd) {
+		if(kwd.equals(name)) return true;
+		return false;
+	}
+
+}
